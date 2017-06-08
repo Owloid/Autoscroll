04651457e66714139cd017b2306e10a4ba1ba132
Jarett Lee <jarett.lee.pi@gmail.com>
2017-06-08 02:00:17 -0400

    Fix background not calling Autoscroll
    When the browser action was called, scroll.js failed to load because
    the name of the file was autoscroll.js.

--- Autoscroll/autoscroll.js
+++ Autoscroll/autoscroll.js
@@ -4,46 +4,46 @@ var SCROLL = SCROLL;
 
 if (SCROLL === undefined) {
 	SCROLL = {};
-	
+
 	var myreq = null;
-	
+
 	var rate = 20;
 	var time;
 	var expectedY;
 	var decimal;
-	
+
 	function start() {
 		if (myreq) return;
-		
+
 		myreq = window.requestAnimationFrame(init);
 	}
-	
+
 	function end() {
 		if (myreq === null) return;
-		
+
 		window.cancelAnimationFrame(myreq);
 		myreq = null;
 	}
-	
+
 	function toggle() {
 		(myreq == null ? start : end)();
 	}
-	
+
 	// doesn't require blocking since it is only used once in the loop
 	function setRate(new_rate) {
 		if (!isNaN(new_rate)) {
 			rate = new_rate;
 		}
 	}
-	
+
 	function init(timestamp) {
 		time = timestamp;
 		expectedY = window.scrollY;
 		decimal = 0;
-		
+
 		myreq = window.requestAnimationFrame(loop);
 	}
-	
+
 	function loop(timestamp) {
 		if (expectedY != window.scrollY) {
 			end();
@@ -54,18 +54,18 @@ if (SCROLL === undefined) {
 		var pixels = rate * elapsed + decimal;
 		decimal = pixels % 1;
 		pixels = parseInt(pixels);
-		
+
 		if (pixels != 0) {
 			expectedY += pixels;
 			window.scrollBy(0, pixels);
 		}
-		
+
 		time = timestamp;
 		myreq = window.requestAnimationFrame(loop);
 	}
-	
+
 	SCROLL.start = start;
 	SCROLL.toggle = toggle;
 }
 
-SCROLL.toggle();
+SCROLL.toggle;
